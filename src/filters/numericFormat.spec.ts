ef4d7387b724194211e1111cde3618a2278a4c3b
Eduard Nikolenko <mail@eduardnikolenko.com>
2019-12-10 01:19:12 +0300

    Full test coverage

--- src/filters/numericFormat.spec.ts
+++ src/filters/numericFormat.spec.ts
@@ -7,17 +7,31 @@ const macro = (t, input: number, config: INumericFormatConfig, expected: string)
   t.is(numericFormat(input, config), expected);
 };
 
-const numericFormatConfig: INumericFormatConfig = {
+const numericFormatConfig1: INumericFormatConfig = {
   decimalSeparator: ',',
   fractionDigitsMax: 2,
   fractionDigitsMin: 2,
   thousandsDigitsSeparator: ' '
 };
 
-test('1000000 => 1 000 000,00', macro, 1000000, numericFormatConfig, '1 000 000,00');
+const numericFormatConfig2: INumericFormatConfig = {
+  ...numericFormatConfig1,
+  fractionDigitsMin: 0
+};
 
-test('1000000.01 => 1 000 000,01', macro, 1000000.01, numericFormatConfig, '1 000 000,01');
+const numericFormatConfig3: INumericFormatConfig = {
+  ...numericFormatConfig1,
+  fractionDigitsMax: 0
+};
 
-test('1000000.67 => 1 000 000,67', macro, 1000000.67, numericFormatConfig, '1 000 000,67');
+test('1000000 => 1 000 000,00', macro, 1000000, numericFormatConfig1, '1 000 000,00');
 
-test('1000000.001 => 1 000 000,00', macro, 1000000.001, numericFormatConfig, '1 000 000,00');
+test('1000000 => 1 000 000 test fractionDigitsMin = 0', macro, 1000000, numericFormatConfig2, '1 000 000');
+
+test('1000000 => 1 000 000 test fractionDigitsMax = 0', macro, 1000000, numericFormatConfig3, '1 000 000');
+
+test('1000000.01 => 1 000 000,01', macro, 1000000.01, numericFormatConfig1, '1 000 000,01');
+
+test('1000000.67 => 1 000 000,67', macro, 1000000.67, numericFormatConfig1, '1 000 000,67');
+
+test('1000000.001 => 1 000 000,00', macro, 1000000.001, numericFormatConfig1, '1 000 000,00');
