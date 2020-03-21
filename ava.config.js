8ee848079055b3ee63943916cca0740f0f4fb3b8
Eduard Nikolenko <mail@eduardnikolenko.com>
2020-03-21 23:37:59 +0300

    Bump deps

--- ava.config.js
+++ ava.config.js
@@ -1,19 +1,17 @@
 export default {
   cache: true,
   concurrency: 4,
-  extensions: [
-    'ts',
-    'vue'
-  ],
   files: [
     'src/**/*.spec.ts'
   ],
   ignoredByWatcher: [
-    '!rc/**/*.spec.ts'
+    '!src/**/*.spec.ts'
   ],
   require: [
     'ts-node/register',
     './ava.setup.js'
   ],
-  typescript: true
+  typescript: {
+    rewritePaths: {}
+  }
 };
